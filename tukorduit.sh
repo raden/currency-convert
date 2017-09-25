dabebb6614ed0570da6e621227b5296e0092850f
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-09-25 11:09:52 +0800

    Revamp a bit on the script

--- tukorduit.sh
+++ tukorduit.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-#PROGNAME=`basename $0`
-#re='^[0-9]+([.][0-9]+)?$'
+PROGNAME=`basename $0`
+re='^[0-9]+([.][0-9]+)?$'
 
-#usage() {
-#	printf "\nUsage:\n"
-#	printf " $PROGNAME <number with or without decimal> <intended currency> <source currency>\n" 
-#}
+usage() {
+	printf "\nUsage:\n"
+	printf " $PROGNAME <number with or without decimal> <source currency> <intended currency> \n" 
+}
 
 tukorduit(){
 
 currency=$(curl "http://finance.google.com/finance/converter?a=$1&from=$2&to=$3" 2>/dev/null |grep class=bld|awk -F"\<\|\>" 2>/dev/null {'print $3, $5'})
 
 
-#if ! [[ $1 =~ $re ]] ; then
-#	printf "Please insert a valid number or decimal if needed\n" >&2
+if ! [[ $1 =~ $re ]] ; then
+	printf "Please insert a valid number or decimal if needed\n" >&2
 
-if [[ $1 -ne 1 ]]; then
-#elif [[ $1 -ne 1 ]]; then
+elif  [[ -z $1  ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
+	usage
+	exit 0
+
+elif [[ $1 -ne 1 ]]; then
 	printf "$currency\n"
-	convert=$(echo $currency|awk {'print $3'})
-	printf "$convert\n"
-#	printf "Nilai untuk 1 ${2^^} ialah `echo "scale=4;$convert/$1"|bc` ${3^^}\n"
-
+	convert=$(echo $currency|awk {'print $4'})
+	printf "Nilai untuk 1 ${2^^} ialah `echo "scale=4;$convert/$1"|bc` ${3^^}\n"
 else
-
 	printf "$currency\n"
 
 fi
 
 }
 
-tukorduit
+tukorduit $1 $2 $3
