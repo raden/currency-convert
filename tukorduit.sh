6bb6ea98e7250ccbd95efd2f793be38ea26456c6
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-09-27 12:55:33 +0800

    Updated case checks

--- tukorduit.sh
+++ tukorduit.sh
@@ -16,14 +16,21 @@ currency=$(curl "http://finance.google.com/finance/converter?a=$1&from=$2&to=$3"
 if ! [[ $1 =~ $re ]] ; then
 	printf "Please insert a valid number or decimal if needed\n" >&2
 
-elif  [[ -z $1  ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
+elif  [[ -z $1  || -z $2 || -z $3 ]]; then
 	usage
 	exit 0
 
 elif [[ $1 -ne 1 ]]; then
 	printf "$currency\n"
 	convert=$(echo $currency|awk {'print $4'})
-	printf "Nilai untuk 1 ${2^^} ialah `echo "scale=4;$convert/$1"|bc` ${3^^}\n"
+	if [[ $2 == *[a-z]* ]] || [[ $3 == *[a-z]* ]]; then 
+		new2=$(echo $2|tr [a-z] [A-Z])
+		new3=$(echo $3|tr [a-z] [A-Z])
+		printf "Nilai untuk 1 $new2 ialah `echo "scale=4;$convert/$1"|bc` $new3\n"
+	else
+		printf "Nilai untuk 1 $2 ialah `echo "scale=4;$convert/$1"|bc` $3\n"
+	fi
+
 else
 	printf "$currency\n"
 
