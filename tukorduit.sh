ae940d0e1da91e7e87e017e5c42d9a4b8ba93465
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2018-03-27 23:36:41 +0800

    Mac OS doesn't accept ^^ sign for uppercase so I use tr instead

--- tukorduit.sh
+++ tukorduit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 # Written by Muhammad Najmi Ahmad Zabidi
 PROGNAME=`basename $0`
 re='^[0-9]+([.][0-9]+)?$'
@@ -9,6 +9,11 @@ usage() {
 }
 
 tukorduit(){
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 currency=$(lynx -dump https://www.google.com/search?q=$1$2+$3|grep "Malaysian Ringgit"|head -1|awk {'print $1,$5'})
 currency2=$(echo $currency|sed 's/,//g'|awk -F " " {'print $1'})
@@ -23,11 +28,15 @@ elif  [[ -z $1  || -z $2 || -z $3 ]]; then
 	exit 0
 
 elif [[ $1 -ne 1 ]]; then
+       new2=$(echo $2|tr '[:lower:]' '[:upper:]')
+       new3=$(echo $3|tr '[:lower:]' '[:upper:]')
         echo ""
         echo -e "${YELLOW}------------KADAR TUKARAN PASARAN----------${NC}"
-        printf "Nilai utk $currency2 ${2^^} ialah $currency3 ${3^^}\n"
+#        printf "Nilai utk $currency2 ${2^^} ialah $currency3 ${3^^}\n"
+        printf "Nilai utk $currency2 $new2 ialah $currency3 $new3\n"
         echo -n
-        echo -e "Nilai untuk 1 ${2^^} ialah ${GREEN}${3^^} $currency4\n${NC}"
+#        echo -e "Nilai untuk 1 ${2^^} ialah ${GREEN}${3^^} $currency4${NC}"
+        echo -e "Nilai untuk 1 $new2 ialah ${GREEN}$new3 $currency4${NC}\n"
 	exit 0
 
 else
