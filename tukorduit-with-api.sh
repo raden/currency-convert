93175d4bcf011e2d97ff33c316884e32bec976fe
Najmi <najmi.zabidi@gmail.com>
2018-07-15 22:52:58 +0800

    Added color, more elegant reporting

--- tukorduit-with-api.sh
+++ tukorduit-with-api.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -l
+# Written by Muhammad Najmi Ahmad Zabidi, 2018
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 if [ -z `which jq` ]; then
 	printf "You need to install jq, a JSON parsing tool \n"
@@ -13,8 +19,12 @@ sumber=$(curl https://exchangeratesapi.io/api/latest?base=$sourcemoney 2>/dev/nu
 
 jumlah=$(echo "$1*$sumber"|bc)
 
-printf "Price per unit: $sumber\n"
-printf "Source monies: $sourcemoney $1\n"
-printf "Total monies after the conversion: $target $jumlah \n"
-printf "Thank you! \n"
+echo -e "Price per unit: ${GREEN}1 $sourcemoney${NC} =  ${YELLOW}$sumber $target${NC}"
+echo -n
+
+echo -e "Source monies: ${YELLOW}$sourcemoney $1${NC}"
+echo -n
+
+echo -e "Total monies after the conversion: ${YELLOW}$target $jumlah${NC}"
+echo -n
 
