afa05dd364a03c64e83fa03cff29f7d4bba9e3b7
Muhammad Najmi Ahmad Zabidi <raden@users.noreply.github.com>
2019-12-14 22:07:20 +0800

    Update tukorduit.sh
    Adapted to Google's changes

--- tukorduit.sh
+++ tukorduit.sh
@@ -16,9 +16,9 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 currency=$(lynx -dump https://www.google.com/search?q=$1$2+$3|grep "Malaysian Ringgit"|head -1|awk {'print $1,$6'})
-currency2=$(echo $currency|sed 's/,//g'|awk -F " " {'print $1'})
-currency3=$(echo $currency|sed 's/,//g'|awk -F " " {'print $2'})
-currency4=$(echo "scale=4;$currency3/$currency2"|bc)
+#currency2=$(echo $currency|sed 's/,//g'|awk -F " " {'print $1'})
+currency3=$(echo $currency|sed 's/,//g'|awk -F " " {'print $1'})
+currency4=$(echo "scale=4;$currency3/$1"|bc)
 
 if ! [[ $1 =~ $re ]] ; then
 	printf "Please insert a valid number or decimal if needed\n" >&2
@@ -33,7 +33,7 @@ elif [[ $1 -ne 1 ]]; then
         echo ""
         echo -e "${YELLOW}------------KADAR TUKARAN PASARAN----------${NC}"
 #        printf "Nilai utk $currency2 ${2^^} ialah $currency3 ${3^^}\n"
-        printf "Nilai utk $currency2 $new2 ialah $currency3 $new3\n"
+        printf "Nilai utk $1 $new2 ialah $currency $new3\n"
         echo -n
 #        echo -e "Nilai untuk 1 ${2^^} ialah ${GREEN}${3^^} $currency4${NC}"
         echo -e "Nilai untuk 1 $new2 ialah ${GREEN}$new3 $currency4${NC}\n"
